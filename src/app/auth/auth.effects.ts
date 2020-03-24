7f25a149dafc839a7b69dd76c797bd925050bac5
jlaguilargomez <jlaguigom@gmail.com>
2020-03-24 10:24:57 +0100

    section 3 -- 2

--- src/app/auth/auth.effects.ts
+++ src/app/auth/auth.effects.ts
@@ -1,13 +1,26 @@
 import { Injectable } from '@angular/core';
-import { Actions } from '@ngrx/effects';
+import { Actions, ofType, createEffect } from '@ngrx/effects';
+import { AuthActions } from './action-types';
+import { tap } from 'rxjs/operators';
 
 @Injectable()
 export class AuthEffects {
+  // mediante este método creamos el observable y nos subscribimos
+  login$ = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType(AuthActions.login),
+        tap(action => localStorage.setItem('user', JSON.stringify(action.user)))
+      ),
+    { dispatch: false }
+  );
+
   constructor(private actions$: Actions) {
-    actions$.subscribe(action => {
-      if (action.type === '[Login Page] User Login') {
-        localStorage.setItem('user', JSON.stringify(action['user']));
-      }
-    });
+    // vamos a realizarlo de una forma más óptima
+    // actions$.subscribe(action => {
+    //   if (action.type === '[Login Page] User Login') {
+    //     localStorage.setItem('user', JSON.stringify(action['user']));
+    //   }
+    // });
   }
 }
